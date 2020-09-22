5ba0158af62da9382199f39de0ddfcfd386217ed
Stanley <stanley.traub@gmail.com>
2020-09-21 21:48:16 -0700

    Initial Commit

---
+++ DesignCodeCourse/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  DesignCodeCourse
+//
+//  Created by Stanley Traub on 9/21/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
